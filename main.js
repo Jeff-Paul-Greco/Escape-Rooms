c003aec023a3c354153f8da35172f47f8fd3c429
Jeff Greco <jeffpgreco@gmail.com>
2020-03-22 20:33:43 -0400

    percy q2 done

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 $(".percyQ1").on("click", function(event) {
     event.preventDefault()
     if ($("#father").val() == "Hermes" && $("#mother").val() == "Aphrodite") {
+        window.location = "percy-question-puzz.html"
+    } else {
+        alert("Try Again!")
+    }
+})
+
+$(".percyQ2").on("click", function(event) {
+    event.preventDefault()
+    if ($("#father").val() == "Chronos") {
         alert("Good Job!")
     } else {
         alert("Try Again!")
